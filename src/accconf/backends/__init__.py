270071ed9cdc92b0d321efa1acb1a78eb995c3d7
Teresia Olsson <teresia.olsson@helmholtz-berlin.de>
2026-04-21 12:38:08 +0200

    Remove yaml backend and add json backend instead.

--- src/accconf/backends/__init__.py
+++ src/accconf/backends/__init__.py
@@ -7,26 +7,19 @@ logger = logging.getLogger(__name__)
 # TODO: add json backend
 
 def _get_backend(backend):
-    if backend == 'yaml':
-        from .yaml_db import YamlBackend
-        return YamlBackend
-    # if backend == 'json':
-    #     from .json_db import JSONBackend
-    #     return JSONBackend
-    # raise ValueError(f'Unknown backend {backend!r}')
+    if backend == 'json':
+        from .json_db import JSONBackend
+        return JSONBackend
+    raise ValueError(f'Unknown backend {backend!r}')
 
 
 def _get_backends():
     # A hook for entrypoints or something similar later
     backends = {}
     try:
-        backends['yaml'] = _get_backend('yaml')
+        backends['json'] = _get_backend('json')
     except ImportError as ex:
-        logger.debug('YAML backend unavailable: %s', ex)
-    # try:
-    #     backends['json'] = _get_backend('json')
-    # except ImportError as ex:
-    #     logger.debug('JSON backend unavailable: %s', ex)
+        logger.debug('JSON backend unavailable: %s', ex)
 
     return backends
 
@@ -37,7 +30,7 @@ try:
     # environment variable. Import this as the standard backend for other
     # places in the module. A user can always override this by explicitly
     # importing the backend
-    DEFAULT_BACKEND = BACKENDS[os.environ.get("ACCCONF_BACKEND", 'yaml').lower()]
+    DEFAULT_BACKEND = BACKENDS[os.environ.get("ACCCONF_BACKEND", 'json').lower()]
 except KeyError:
     raise ImportError("Improper specification of AccConf backend. "
                       "Check the `$ACCCONF_BACKEND` environment variable.")
